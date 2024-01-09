5d9f4151b86af1932c98e9bfa0c1685ed93ca9b5
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-01-09 17:03:46 +0500

    update design for home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from 'next'
+import { Geologica } from 'next/font/google'
+const first_font = Geologica({ subsets: ['latin', 'cyrillic'], weight: ['600', '500', '400', '300', '200'], variable: '--root-font' })
 
 export const metadata: Metadata = {
   title: 'DM Calendar',
@@ -15,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={first_font.className}>
       <body className='dark'>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,100 @@
-import Header from "@/components/widgets/header";
+import { Button } from "@/components/ui/button";
 import { generateMonthCalendar } from "@/helpers/calendar-generators";
 import { DateTime } from "luxon";
+import Link from "next/link";
+import { BiMenu, BiX } from 'react-icons/bi'
 
-export default function Home() {
-    const nowDate = DateTime.now()
+type Props = {
+  searchParams: {
+    side?: boolean
+  }
+}
+export default function Home({ searchParams }: Props) {
+    const nowDate = DateTime.now().setLocale('ru')
     const days = generateMonthCalendar(nowDate)
+    const currentMonth = nowDate.monthLong
+    const year = nowDate.year
+    const enableSideMenu = searchParams.side || false
     return (
         <>
-          <Header />
-          <div style={{ height: 'calc(100vh - 64px)' }} className="w-full h-full shrink-0 grid grid-cols-7 grid-rows-6 overflow-hidden">
+          {/* <Header /> */}
+          <section className="w-full max-w-screen flex items-start flex-row h-screen">
             {
-              days.map((day, index) => {
-                const dayNum = day.date.day
-                const isToday = day.key === nowDate.toFormat('yyyy-MM-dd')
-                const notCurrentMonth = day.date.month !== nowDate.month
-                const isLastDay = day.date.daysInMonth === day.date.day
-                const isFirstDay = day.date.day === 1
-                return (
-                  <div key={day.date.toString()} className="w-full h-full shrink-0 border-r border-b">
-                    <div className="w-full h-fit pt-2 px-2 flex items-center justify-between">
-                      <span className='text-xs capitalize text-muted-foreground'>{dayNum}</span>
-                      <div className="flex items-center gap-1 w-fit h-fit">
-                          <span className='text-xs capitalize text-muted-foreground'>
-                          {
-                              isToday 
-                              ? 'Сегодня' 
-                              : ((notCurrentMonth && isFirstDay) || (isLastDay && notCurrentMonth)) && <span className="capitalize">{ day.date.setLocale('ru').monthLong }</span>
-                          }
-                          </span>
-                          {
-                              (index >= 0 && index <= 6) && <span className='text-xs capitalize text-muted-foreground'>{ day.date.setLocale('ru').weekdayShort }</span>
-                          }
+              enableSideMenu &&
+              <div className="w-80 shrink-0 h-full bg-card"></div>
+            }
+            <div style={{ width: enableSideMenu ? 'calc(100% - 20rem)' : '' }} className="w-full h-full">
+              <div className="w-full h-16 px-3 flex items-center shrink-0 border-b border-x bg-card">
+                <div className="w-fit h-fit flex items-center gap-3">
+                  <Button size='icon' variant='ghost'>
+                    <Link href={enableSideMenu ? '/' : '?side=true'}>
+                      {
+                        enableSideMenu
+                        ? <BiX size={24} />
+                        : <BiMenu size={24} />
+                      }
+                    </Link>
+                  </Button>
+                  <span className="text-3xl font-bold text-muted-foreground"><span className="text-accent-foreground capitalize">{currentMonth}</span> {year}</span>
+                </div>
+              </div>
+              <div className="w-full h-10 shrink-0 grid grid-cols-7 grid-rows-1 border-b border-x bg-card">
+                <div className="w-full h-full py-2 px-3">
+                  <span className="text-sm text-muted-foreground">Понедельник</span>
+                </div>
+                <div className="w-full h-full py-2 px-3">
+                  <span className="text-sm text-muted-foreground">Вторник</span>
+                </div>
+                <div className="w-full h-full py-2 px-3">
+                  <span className="text-sm text-muted-foreground">Среда</span>
+                </div>
+                <div className="w-full h-full py-2 px-3">
+                  <span className="text-sm text-muted-foreground">Четверг</span>
+                </div>
+                <div className="w-full h-full py-2 px-3">
+                  <span className="text-sm text-muted-foreground">Пятница</span>
+                </div>
+                <div className="w-full h-full py-2 px-3">
+                  <span className="text-sm text-muted-foreground">Суббота</span>
+                </div>
+                <div className="w-full h-full py-2 px-2">
+                  <span className="text-sm text-muted-foreground">Воскресенье</span>
+                </div>
+              </div>
+              <div style={{ height: 'calc(100% - 64px - 40px)' }} className="w-full h-full shrink-0 grid grid-cols-7 grid-rows-6 border-l overflow-hidden">
+                {
+                  days.map((day, index) => {
+                    const dayNum = day.date.day
+                    const isToday = day.key === nowDate.toFormat('yyyy-MM-dd')
+                    const notCurrentMonth = day.date.month !== nowDate.month
+                    const isLastDay = day.date.daysInMonth === day.date.day
+                    const isFirstDay = day.date.day === 1
+                    return (
+                      <div key={day.date.toString()} className="w-full h-full shrink-0 border-r border-b">
+                        <div className="w-full h-fit pt-2 px-3 flex items-center justify-between">
+                          <span className='text-xs capitalize text-muted-foreground'>{dayNum}</span>
+                          <div className="flex items-center gap-1 w-fit h-fit">
+                              <span className='text-xs capitalize text-muted-foreground'>
+                              {
+                                  isToday 
+                                  ? 'Сегодня' 
+                                  : ((notCurrentMonth && isFirstDay) || (isLastDay && notCurrentMonth)) && <span className="capitalize">{ day.date.setLocale('ru').monthLong }</span>
+                              }
+                              </span>
+                              {
+                                  (index >= 0 && index <= 6) && <span className='text-xs capitalize text-muted-foreground'>{ day.date.setLocale('ru').weekdayShort }</span>
+                              }
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
+                      )
+                    }
                   )
                 }
-              )
-            }
-          </div>
+              </div>
+            </div>
+          </section>
+
         </>
     )
 }
